6b8f241f5c208e876b76a8af7cc44f990dbe5d18
jebastinherlen <herlenjebastin@gmail.com>
2026-05-27 20:52:59 +0530

    axios code change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",   // ✔ Correct API URL
+  baseURL: "http://188.34.166.115/api",   // ✔ Correct API URL
   headers: { "Content-Type": "application/json" },
 });
 
