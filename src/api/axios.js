4cd4fff6463b373dbd2b039b84767d8e16962c54
jebastinherlen <herlenjebastin@gmail.com>
2026-07-14 16:02:34 +0530

    baseURL bug change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://masmap-backend.vercel.app//api",
+  baseURL: "https://masmap-backend.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
